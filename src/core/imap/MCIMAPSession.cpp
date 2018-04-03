addc3f278e7132039cf29c9f69f95d9c75673c61
Patrick Rogers <progers@alumni.uwaterloo.ca>
2018-04-03 18:25:42 -0400

    Exposed the isMoveEnabled property from low level mail core to the objective C wrapper. MCIMAPSession was configuring an index property for the move capability, but it wasn't reporting it through its public API, and thus MCIMAPAsyncSession and MCOIMAPSession were not exposing it.

--- src/core/imap/MCIMAPSession.cpp
+++ src/core/imap/MCIMAPSession.cpp
@@ -382,6 +382,7 @@ void IMAPSession::init()
     mNamespaceEnabled = false;
     mCompressionEnabled = false;
     mIsGmail = false;
+    mIsMoveEnabled = false;
     mAllowsNewPermanentFlags = false;
     mWelcomeString = NULL;
     mNeedsMboxMailWorkaround = false;
@@ -4254,6 +4255,9 @@ void IMAPSession::applyCapabilities(IndexSet * capabilities)
         mIdleEnabled = true;
         UNLOCK();
     }
+    if (capabilities->containsIndex(IMAPCapabilityMove)) {
+        mIsMoveEnabled = true;
+    }
     if (capabilities->containsIndex(IMAPCapabilityCondstore)) {
         mCondstoreEnabled = true;
     }
@@ -4329,6 +4333,11 @@ bool IMAPSession::allowsNewPermanentFlags() {
 bool IMAPSession::isGmail()
 {
     return mIsGmail;
+}
+
+bool IMAPSession::isMoveEnabled()
+{
+    return mIsMoveEnabled;
 }
 
 bool IMAPSession::isDisconnected()
