b1ddd41e97d04f28d647a69653ee1b9fbf754f02
Adam Tharani <adamtharani@me.com>
2021-05-17 15:17:08 -0400

    Added a lock to the dependancy fetch script so that multiple instances can't run at the same time

--- scripts/get-ios.sh
+++ scripts/get-ios.sh
@@ -6,7 +6,7 @@ popd > /dev/null
 
 . "$scriptpath/include.sh/build-dep.sh"
 
-acquire_lock "$0.lock"
+acquire_lock "$scriptpath/$0.lock"
 
 deps="ctemplate-ios libetpan-ios tidy-html5-ios"
 for dep in $deps ; do
